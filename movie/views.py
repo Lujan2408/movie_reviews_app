beb701ab53cbedebf9945a9396e3da59769d6584
Jose David <lujan9495@gmail.com>
2025-06-21 15:33:57 -0500

    Generating HTML Pages with Templates and passing data into the templates

--- movie/views.py
+++ movie/views.py
@@ -2,7 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request): 
-  return HttpResponse('<h1>Welcome to Home page!!</h1>')
+  return render(request, 'home.html', 
+    {
+      'name': 'Jose'
+    }
+  )
 
 def about(request):
   return HttpResponse('<h1>Welcome to About Page</h1>')

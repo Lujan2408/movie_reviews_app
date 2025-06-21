f0c4d8fce0e9e0a85b54542e5477bc016a468250
Jose David <lujan9495@gmail.com>
2025-06-21 15:07:01 -0500

    creating an about path

--- movie/views.py
+++ movie/views.py
@@ -3,3 +3,6 @@ from django.http import HttpResponse
 
 def home(request): 
   return HttpResponse('<h1>Welcome to Home page!!</h1>')
+
+def about(request):
+  return HttpResponse('<h1>Welcome to About Page</h1>')

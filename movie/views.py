2258640d8cf07507f6dfe96830917ba7562effd8
Jose David <lujan9495@gmail.com>
2025-06-21 20:42:30 -0500

    Implementing a search filter with case-sensitive match

--- movie/views.py
+++ movie/views.py
@@ -4,12 +4,17 @@ from .models import Movie
 
 def home(request):
   searchTerm = request.GET.get('searchMovie')
-  movies = Movie.objects.all()
-  
+  if searchTerm: 
+#   If a search term is entered, we call the model's filter method to return the movie 
+#   objects with a case-insensitive match to the search term:
+    movies = Movie.objects.filter(title__icontains=searchTerm)
+  else: 
+    movies = Movie.objects.all()
   return render(request, 'home.html', {
     'searchTerm' : searchTerm,
-    'movies': movies
+    'movies' : movies
   })
+  
 
 def about(request):
   return HttpResponse('<h1>Welcome to About Page</h1>')

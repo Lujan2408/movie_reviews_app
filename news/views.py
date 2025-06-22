a2d2a9a2afa96410601e3d1fa85f8a4328a7be1b
Jose David <lujan9495@gmail.com>
2025-06-21 21:32:16 -0500

    Displaying most recent news first

--- news/views.py
+++ news/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from .models import New
 
 def news(request): 
-  news = New.objects.all()
+  news = New.objects.all().order_by('-date') # Display the most recent new
   return render(request, "news.html", {
     'news': news
   })

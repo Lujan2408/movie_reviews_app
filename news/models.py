677c11865833d657c842cb09b1a5e258c5891b5f
Jose David <lujan9495@gmail.com>
2025-06-21 21:11:24 -0500

    Creating the News model and register in admin interface

--- news/models.py
+++ news/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class New(models.Model): 
+  headline = models.CharField(max_length=200)
+  body = models.TextField()
+  date = models.DateField()
+
+# To make migrations: 
+# python manage.py makemigrations
+# python manage.py migrate

a86d77a54a66f5b50d613220604385f6483fde2c
Jose David <lujan9495@gmail.com>
2025-06-21 22:39:27 -0500

    Using a function to  improve readability in admin interface

--- news/models.py
+++ news/models.py
@@ -5,6 +5,10 @@ class New(models.Model):
   body = models.TextField()
   date = models.DateField()
 
+  #  The __str__ method represents the class objects as a string: __str__ will be called when the news objects are listed in admin
+  def __str__(self):
+    return self.headline
+
 # To make migrations: 
 # python manage.py makemigrations
 # python manage.py migrate

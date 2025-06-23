12d977d86cc4a2c145695a120c756e5dcc593c51
Jose David <lujan9495@gmail.com>
2025-06-23 14:13:20 -0500

    Creating a user

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.contrib.auth import login
 
 def signupaccount(request):
-  return render(request, 'signupaccount.html', {
-    'form': UserCreationForm
-  })
+  # If there´s a GET request it means that there's a user navigating to the sign up form via URL(localhost:8000/accounts/signupaccount) then we send them to the login form 
+  if request.method == 'GET':
+    return render(request, 'signupaccount.html', {
+      'form': UserCreationForm
+    })
+  else: 
+    if request.POST['password1'] == request.POST['password2'] : 
+      user = User.objects.create_user(request.POST['username'], password=request.POST['password1'])
+      user.save()
+      login(request, user)
+      return redirect('home')
+      

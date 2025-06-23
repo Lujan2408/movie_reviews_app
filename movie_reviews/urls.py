c6f4a377c5c3965f9e7e46e6d45d270597e20229
Jose David <lujan9495@gmail.com>
2025-06-23 13:54:44 -0500

    Created the accounts url. Created views and signuppaccount url and its template NOTE:  "{% csrf_token %}" in sign up template; this to protect our form from cross-site request forgery (CSRF) attacks.

--- movie_reviews/urls.py
+++ movie_reviews/urls.py
@@ -28,7 +28,8 @@ urlpatterns = [
     path('about/', movieViews.about),
     path('signUp', movieViews.signUp, name='signUp'),
     path('news/', include('news.urls')), # path('news/', include('news.url')) will forward any requests with 'news/' to the news app's urls.py.
-    path('movie/', include('movie.urls'))
+    path('movie/', include('movie.urls')),
+    path('accounts/', include('accounts.urls'))
 ]
 
 # With this, you can serve the static media from Django.

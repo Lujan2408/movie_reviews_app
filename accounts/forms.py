0bbeb95917acddb29162ebda5d753b07b617a473
Jose David <lujan9495@gmail.com>
2025-06-23 15:22:13 -0500

    Created a form files to handle custom forms. Customizing UserCreationForm

---
+++ accounts/forms.py
@@ -0,0 +1,14 @@
+from django.contrib.auth.forms import UserCreationForm
+
+# We create a new form that extends from UserCreationForm
+
+#  We will also set help_text of the form's fields to None to remove them: self.fields[fieldname].help_text = None
+
+class UserCreateForm(UserCreationForm): 
+  def __init__(self, *args, **kwargs): 
+    super(UserCreateForm, self).__init__(*args, **kwargs)
+    for fieldname in ['username', 'password1', 'password2']:
+      self.fields[fieldname].help_text = None # type: ignore
+      self.fields[fieldname].widget_attrs.update({ # type: ignore
+        'class': 'form-control'
+      })
